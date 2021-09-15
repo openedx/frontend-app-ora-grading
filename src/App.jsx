dacc6333ae8904a554db9cdf6e865c4175733b09
Leangseu Kim <lkim@edx.org>
2021-09-15 15:17:40 -0400

    commit before updating test
    updated some test

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { IntlProvider } from 'react-intl';
 
 import Footer from '@edx/frontend-component-footer';
+import { IntlProvider } from '@edx/frontend-platform/i18n';
 
 import { routePath } from 'data/constants/app';
 import store from 'data/store';
 import ListView from 'containers/ListView';
-//import messages from './i18n';
+import messages from './i18n';
 import './App.scss';
 
+import { AppProvider, ErrorPage, PageRoute } from '@edx/frontend-platform/react';
+import Header from './course-header/Header';
+
+
 const App = () => (
-  // <IntlProvider locale="en" messages={messages.en} >
-    <Provider store={store}>
+  <IntlProvider locale="en" messages={messages.en} >
+    <AppProvider store={store}>
       <Router>
         <div>
+          <Header
+            courseOrg='Course org'
+            courseNumber='Course Number'
+            courseTitle='Course Title'
+          />
           <main>
             <ListView />
           </main>
-
           <Footer logo={process.env.LOGO_POWERED_BY_OPEN_EDX_URL_SVG} />
-
         </div>
       </Router>
-    </Provider>
-  // </IntlProvider>
+    </AppProvider>
+  </IntlProvider>
 );
 
 export default App;
