db0598b4e95cc7815437fb952f24dc3046bebc2d
Ben Warzeski <bwarzeski@edx.org>
2022-04-22 00:40:03 -0400

    chore: api tests

---
+++ src/data/services/lms/api.test.js
@@ -0,0 +1,140 @@
+import { StrictDict, keyStore } from 'utils';
+import { locationId } from 'data/constants/app';
+import { paramKeys } from './constants';
+import urls from './urls';
+import api from './api';
+import { stringifyUrl } from './utils';
+
+jest.mock('./utils', () => ({
+  client: () => ({ delete: (url) => Promise.resolve({ data: { delete: { url } } }) }),
+  get: (url) => Promise.resolve({ data: { get: { url } } }),
+  post: (url, data) => Promise.resolve({ data: { post: { url, data } } }),
+  stringifyUrl: args => ({ stringifyUrl: args }),
+}));
+
+jest.mock('data/constants/app', () => ({
+  locationId: 'test-location-id',
+}));
+
+const gradeData = 'test-grade-data';
+const submissionUUID = 'test-submission-uuid';
+const submissionUUIDs = ['some', 'submission', 'uuid'];
+
+const methodKeys = StrictDict({
+  get: 'get',
+  post: 'post',
+  delete: 'delete',
+});
+
+const urlKeys = keyStore(urls);
+
+const testAPI = ({
+  promise,
+  method,
+  expected: {
+    urlKey,
+    urlParams,
+    ...otherExpected
+  },
+}) => {
+  it(`returns ${method}(${urlKey}) with correct args and reoslves with response data`, () => (
+    promise.then((data) => {
+      expect(data[method]).toEqual({
+        url: stringifyUrl(urls[urlKey], urlParams),
+        ...otherExpected,
+      });
+    })
+  ));
+};
+
+describe('lms service api methods', () => {
+  describe('initializeApp', () => {
+    testAPI({
+      promise: api.initializeApp(),
+      method: methodKeys.get,
+      expected: {
+        urlKey: urlKeys.oraInitializeUrl,
+        urlParams: { [paramKeys.oraLocation]: locationId },
+      },
+    });
+  });
+  describe('fetchSubmission', () => {
+    testAPI({
+      promise: api.fetchSubmission(submissionUUID),
+      method: methodKeys.get,
+      expected: {
+        urlKey: urlKeys.fetchSubmissionUrl,
+        urlParams: {
+          [paramKeys.oraLocation]: locationId,
+          [paramKeys.submissionUUID]: submissionUUID,
+        },
+      },
+    });
+  });
+  describe('fetchSubmissionStatus', () => {
+    testAPI({
+      promise: api.fetchSubmissionStatus(submissionUUID),
+      method: methodKeys.get,
+      expected: {
+        urlKey: urlKeys.fetchSubmissionStatusUrl,
+        urlParams: {
+          [paramKeys.oraLocation]: locationId,
+          [paramKeys.submissionUUID]: submissionUUID,
+        },
+      },
+    });
+  });
+  describe('lockSubmission', () => {
+    testAPI({
+      promise: api.lockSubmission(submissionUUID),
+      method: methodKeys.post,
+      expected: {
+        urlKey: urlKeys.fetchSubmissionLockUrl,
+        urlParams: {
+          [paramKeys.oraLocation]: locationId,
+          [paramKeys.submissionUUID]: submissionUUID,
+        },
+      },
+    });
+  });
+  describe('unlockSubmission', () => {
+    testAPI({
+      promise: api.unlockSubmission(submissionUUID),
+      method: methodKeys.delete,
+      expected: {
+        urlKey: urlKeys.fetchSubmissionLockUrl,
+        urlParams: {
+          [paramKeys.oraLocation]: locationId,
+          [paramKeys.submissionUUID]: submissionUUID,
+        },
+      },
+    });
+  });
+  describe('batchUnlockSubmissions', () => {
+    testAPI({
+      promise: api.batchUnlockSubmissions(submissionUUIDs),
+      method: methodKeys.post,
+      expected: {
+        urlKey: urlKeys.batchUnlockSubmissionsUrl,
+        urlParams: {
+          [paramKeys.oraLocation]: locationId,
+        },
+        data: { submissionUUIDs },
+      },
+    });
+  });
+  describe('updateGrade', () => {
+    testAPI({
+      promise: api.updateGrade(submissionUUID, gradeData),
+      method: methodKeys.post,
+      expected: {
+        urlKey: urlKeys.updateSubmissionGradeUrl,
+        urlParams: {
+          [paramKeys.oraLocation]: locationId,
+          [paramKeys.submissionUUID]: submissionUUID,
+        },
+        data: gradeData,
+      },
+    });
+  });
+});
