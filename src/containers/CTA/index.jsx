dc13d4095c4066aded1a18d218d83445f8d0d4ea
Leangseu Kim <lkim@edx.org>
2022-05-16 12:56:55 -0400

    chore: update hyperlink style

--- src/containers/CTA/index.jsx
+++ src/containers/CTA/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
-import { PageBanner } from '@edx/paragon';
+import { PageBanner, Hyperlink } from '@edx/paragon';
 
 import messages from './messages';
 
@@ -9,16 +9,18 @@ import messages from './messages';
  * <CTA />
  */
 export const CTA = () => (
-  <PageBanner variant="light">
+  <PageBanner>
     <span>
       <FormattedMessage {...messages.ctaFeedbackMessage} />&nbsp;
-      <a
-        href="https://docs.google.com/forms/d/1Hu1rgJcCHl5_EtDb5Up3hiZ40sSUtkZQfRHJ3fWOvfQ/edit"
+      <Hyperlink
+        isInline
+        variant="muted"
+        destination="https://docs.google.com/forms/d/1Hu1rgJcCHl5_EtDb5Up3hiZ40sSUtkZQfRHJ3fWOvfQ/edit"
         target="_blank"
-        rel="noopener noreferrer"
+        showLaunchIcon={false}
       >
         <FormattedMessage {...messages.ctaLinkMessage} />
-      </a>
+      </Hyperlink>
       .
     </span>
   </PageBanner>
