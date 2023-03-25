06aea1ff682f4938b7767ddca3f4178879560774
Yoiber <105317298+Yoiber071@users.noreply.github.com>
2023-03-25 12:00:56 -0500

    chore(i18n): add more languages (#160)

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -3,12 +3,26 @@ import arMessages from './messages/ar.json';
 import es419Messages from './messages/es_419.json';
 import frMessages from './messages/fr.json';
 import zhcnMessages from './messages/zh_CN.json';
+import dedeMessages from './messages/de_DE.json';
+import frcaMessages from './messages/fr_CA.json';
+import hiMessages from './messages/hi.json';
+import ititMessages from './messages/it_IT.json';
+import ptptMessages from './messages/pt_PT.json';
+import ruMessages from './messages/ru.json';
+import ukMessages from './messages/uk.json';
 
 const messages = {
   ar: arMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
+  'de-de': dedeMessages,
+  'fr-ca': frcaMessages,
+  hi: hiMessages,
+  'it-it': ititMessages,
+  'pt-pt': ptptMessages,
+  ru: ruMessages,
+  uk: ukMessages,
 };
 
 export default messages;
