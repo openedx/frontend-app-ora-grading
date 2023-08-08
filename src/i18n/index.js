fdb0f6378edf5a63422f07b2343ebe399f5b19f8
Emad Ehsanrad <codewithemad@gmail.com>
2023-08-08 19:56:49 +0330

    chore: camelCase variable used for translated messages

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,32 +3,32 @@ import { messages as headerMessages } from '@edx/frontend-component-header';
 import { messages as paragonMessages } from '@edx/paragon';
 
 import arMessages from './messages/ar.json';
-import dedeMessages from './messages/de_DE.json';
+import deDEMessages from './messages/de_DE.json';
 import es419Messages from './messages/es_419.json';
 import faIRMessages from './messages/fa_IR.json';
 import frMessages from './messages/fr.json';
-import frcaMessages from './messages/fr_CA.json';
+import frCAMessages from './messages/fr_CA.json';
 import hiMessages from './messages/hi.json';
-import ititMessages from './messages/it_IT.json';
-import ptptMessages from './messages/pt_PT.json';
+import itITMessages from './messages/it_IT.json';
+import ptPTMessages from './messages/pt_PT.json';
 import ruMessages from './messages/ru.json';
 import ukMessages from './messages/uk.json';
-import zhcnMessages from './messages/zh_CN.json';
+import zhCNMessages from './messages/zh_CN.json';
 // no need to import en messages-- they are in the defaultMessage field
 
 const appMessages = {
   ar: arMessages,
-  'de-de': dedeMessages,
+  'de-de': deDEMessages,
   'es-419': es419Messages,
   'fa-ir': faIRMessages,
   fr: frMessages,
-  'fr-ca': frcaMessages,
+  'fr-ca': frCAMessages,
   hi: hiMessages,
-  'it-it': ititMessages,
-  'pt-pt': ptptMessages,
+  'it-it': itITMessages,
+  'pt-pt': ptPTMessages,
   ru: ruMessages,
   uk: ukMessages,
-  'zh-cn': zhcnMessages,
+  'zh-cn': zhCNMessages,
 };
 
 export default [
