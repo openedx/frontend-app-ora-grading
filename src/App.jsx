36d676b064bb18fae3480ff18f90f25c21353702
Adolfo R. Brandes <adolfo@axim.org>
2024-06-06 15:06:24 -0300

    feat: use frontend-plugin-framework to provide a FooterSlot (#332)
    Co-authored-by: Brian Smith <bsmith@axim.org>

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import Footer from '@edx/frontend-component-footer';
+import FooterSlot from '@openedx/frontend-slot-footer';
 import { LearningHeader as Header } from '@edx/frontend-component-header';
 
 import { selectors } from 'data/redux';
@@ -32,7 +32,7 @@ export const App = ({ courseMetadata, isEnabled }) => (
       <main data-testid="main">
         <ListView />
       </main>
-      <Footer logo={process.env.LOGO_POWERED_BY_OPEN_EDX_URL_SVG} data-testid="footer" />
+      <FooterSlot />
     </div>
   </Router>
 );
