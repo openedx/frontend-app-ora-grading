c9d0abe9683659f4b9e4ccc6f7ee3ea9c55a952d
Asad Ali <asad.ali@arbisoft.com>
2024-11-04 22:22:50 +0500

    fix: convert notification banner to text if accounts url is not set (#362)
    * fix: convert banner to text if ACCOUNT_SETTINGS_URL is not set
    
    * refactor: refactoring
    
    * refactor: rename notificationsBannerLinkMessage to notificationsBannerPreferencesCenterMessage
    
    * refactor: remove lodash usage
    
    * refactor: remove lodash usage

--- src/containers/NotificationsBanner/NotificationsBanner.test.jsx
+++ src/containers/NotificationsBanner/NotificationsBanner.test.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import { shallow } from '@edx/react-unit-test-utils';
+import { getConfig } from '@edx/frontend-platform';
 
 import { NotificationsBanner } from '.';
 
+jest.mock('@edx/frontend-platform', () => ({
+  getConfig: jest.fn(),
+}));
+
 describe('NotificationsBanner component', () => {
-  test('snapshots', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('snapshots with empty ACCOUNT_SETTINGS_URL', () => {
+    getConfig.mockReturnValue({
+      ACCOUNT_SETTINGS_URL: '',
+    });
+    const el = shallow(<NotificationsBanner hide />);
+    expect(el.snapshot).toMatchSnapshot();
+  });
+
+  test('snapshots with ACCOUNT_SETTINGS_URL', () => {
+    getConfig.mockReturnValue({
+      ACCOUNT_SETTINGS_URL: 'http://localhost:1997',
+    });
     const el = shallow(<NotificationsBanner hide />);
     expect(el.snapshot).toMatchSnapshot();
   });
