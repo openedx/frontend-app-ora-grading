cc4b1c8169d1bcc317257504e4a51e7b8c46be98
ayesha waris <73840786+ayesha-waris@users.noreply.github.com>
2025-06-02 23:09:11 +0500

    feat: removed ora staff notification settings banner (#421)
    Co-authored-by: Ayesha Waris <ayesha.waris@192.168.10.31>

--- src/components/ConfirmModal.test.jsx
+++ src/components/ConfirmModal.test.jsx
@@ -1,6 +1,8 @@
-import { shallow } from '@edx/react-unit-test-utils';
-
+import { render } from '@testing-library/react';
 import { ConfirmModal } from './ConfirmModal';
+
+jest.unmock('@openedx/paragon');
+jest.unmock('react');
 
 describe('ConfirmModal', () => {
   const props = {
@@ -12,10 +14,12 @@ describe('ConfirmModal', () => {
     onCancel: jest.fn().mockName('this.props.onCancel'),
     onConfirm: jest.fn().mockName('this.props.onConfirm'),
   };
-  test('snapshot: closed', () => {
-    expect(shallow(<ConfirmModal {...props} />).snapshot).toMatchSnapshot();
+  it('should not render content when modal is closed', () => {
+    const { queryByText } = render(<ConfirmModal {...props} />);
+    expect(queryByText(props.content)).toBeNull();
   });
-  test('snapshot: open', () => {
-    expect(shallow(<ConfirmModal {...props} isOpen />).snapshot).toMatchSnapshot();
+  it('should display content when modal is open', () => {
+    const { getByText } = render(<ConfirmModal {...props} isOpen />);
+    expect(getByText(props.content)).toBeInTheDocument();
   });
 });
