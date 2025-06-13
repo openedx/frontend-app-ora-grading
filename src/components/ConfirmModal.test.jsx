ec3c25f54ac1e8c003b76c7eb95de6be5661826e
Victor Navarro <vm.navarro94@gmail.com>
2025-06-13 05:31:43 -0600

    test: deprecate react-unit-test-utils part-3 (#430)

--- src/components/ConfirmModal.test.jsx
+++ src/components/ConfirmModal.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { ConfirmModal } from './ConfirmModal';
 
 jest.unmock('@openedx/paragon');
@@ -14,12 +14,30 @@ describe('ConfirmModal', () => {
     onCancel: jest.fn().mockName('this.props.onCancel'),
     onConfirm: jest.fn().mockName('this.props.onConfirm'),
   };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should not render content when modal is closed', () => {
     const { queryByText } = render(<ConfirmModal {...props} />);
     expect(queryByText(props.content)).toBeNull();
   });
+
   it('should display content when modal is open', () => {
     const { getByText } = render(<ConfirmModal {...props} isOpen />);
     expect(getByText(props.content)).toBeInTheDocument();
+  });
+
+  it('should call onCancel when cancel button is clicked', () => {
+    render(<ConfirmModal {...props} isOpen />);
+    fireEvent.click(screen.getByText(props.cancelText));
+    expect(props.onCancel).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onConfirm when confirm button is clicked', () => {
+    render(<ConfirmModal {...props} isOpen />);
+    fireEvent.click(screen.getByText(props.confirmText));
+    expect(props.onConfirm).toHaveBeenCalledTimes(1);
   });
 });
