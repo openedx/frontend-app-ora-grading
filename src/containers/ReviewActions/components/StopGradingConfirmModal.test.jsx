45bb716b9da867998e812f6a6167dbe6b0b67516
Victor <vm.navarro94@gmail.com>
2025-06-16 15:51:08 -0600

    test: deprecate react-unit-test-utils part-7

--- src/containers/ReviewActions/components/StopGradingConfirmModal.test.jsx
+++ src/containers/ReviewActions/components/StopGradingConfirmModal.test.jsx
@@ -1,25 +1,60 @@
-import { shallow } from '@edx/react-unit-test-utils';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { IntlProvider } from '@edx/frontend-platform/i18n';
+import StopGradingConfirmModal from './StopGradingConfirmModal';
 
-import { formatMessage } from 'testUtils';
-import { StopGradingConfirmModal } from './StopGradingConfirmModal';
+jest.unmock('@openedx/paragon');
+jest.unmock('react');
 
-jest.mock('components/ConfirmModal', () => 'ConfirmModal');
+const renderWithIntl = (component) => render(
+  <IntlProvider locale="en">
+    {component}
+  </IntlProvider>,
+);
 
 describe('StopGradingConfirmModal', () => {
   const props = {
-    intl: { formatMessage },
     isOpen: false,
     isOverride: false,
     onCancel: jest.fn().mockName('this.props.onCancel'),
     onConfirm: jest.fn().mockName('this.props.onConfirm'),
   };
-  test('snapshot: closed', () => {
-    expect(shallow(<StopGradingConfirmModal {...props} />).snapshot).toMatchSnapshot();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
-  test('snapshot: open', () => {
-    expect(shallow(<StopGradingConfirmModal {...props} isOpen />).snapshot).toMatchSnapshot();
+
+  it('should not render content when modal is closed', () => {
+    const { queryByText } = renderWithIntl(<StopGradingConfirmModal {...props} />);
+    expect(queryByText('Your progress will be lost.')).toBeNull();
   });
-  test('snapshot: open, isOverride', () => {
-    expect(shallow(<StopGradingConfirmModal {...props} isOverride />).snapshot).toMatchSnapshot();
+
+  it('should display stop grading content when modal is open', () => {
+    const { getByText } = renderWithIntl(<StopGradingConfirmModal {...props} isOpen />);
+    expect(getByText('Are you sure you want to stop grading this response?')).toBeInTheDocument();
+    expect(getByText('Your progress will be lost.')).toBeInTheDocument();
+  });
+
+  it('should display stop override content when modal is open and isOverride is true', () => {
+    const { getByText } = renderWithIntl(<StopGradingConfirmModal {...props} isOpen isOverride />);
+    expect(getByText('Are you sure you want to stop grade override?')).toBeInTheDocument();
+    expect(getByText('Your progress will be lost.')).toBeInTheDocument();
+  });
+
+  it('should call onCancel when cancel button is clicked', () => {
+    renderWithIntl(<StopGradingConfirmModal {...props} isOpen />);
+    fireEvent.click(screen.getByText('Go back'));
+    expect(props.onCancel).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onConfirm when confirm button is clicked for regular grading', () => {
+    renderWithIntl(<StopGradingConfirmModal {...props} isOpen />);
+    fireEvent.click(screen.getByText('Cancel grading'));
+    expect(props.onConfirm).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onConfirm when confirm button is clicked for override', () => {
+    renderWithIntl(<StopGradingConfirmModal {...props} isOpen isOverride />);
+    fireEvent.click(screen.getByText('Stop grade override'));
+    expect(props.onConfirm).toHaveBeenCalledTimes(1);
   });
 });
--- src/containers/ReviewActions/components/SubmissionNavigation.test.jsx
+++ src/containers/ReviewActions/components/SubmissionNavigation.test.jsx
@@ -1,76 +1,116 @@
 import React from 'react';
-import { shallow } from '@edx/react-unit-test-utils';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { IntlProvider } from '@edx/frontend-platform/i18n';
 
-import { selectors, thunkActions } from 'data/redux';
-
-import { formatMessage } from 'testUtils';
+import { thunkActions } from 'data/redux';
 
 import {
   SubmissionNavigation,
-  mapStateToProps,
   mapDispatchToProps,
 } from './SubmissionNavigation';
 
-jest.mock('data/redux/grading/selectors', () => ({
-  prev: {
-    doesExist: (state) => ({ prevDoesExist: state }),
+jest.unmock('@openedx/paragon');
+jest.unmock('react');
+
+const mockMessages = {
+  'ora-grading.ReviewActions.loadPrevious': 'Load previous submission',
+  'ora-grading.ReviewActions.loadNext': 'Load next submission',
+  'ora-grading.ReviewActions.navigationLabel': '{current} of {total}',
+};
+
+const mockIntl = {
+  formatMessage: (message, values = {}) => {
+    let text = message.defaultMessage || message.id;
+    Object.keys(values).forEach(key => {
+      text = text.replace(`{${key}}`, values[key]);
+    });
+    return text;
   },
-  next: {
-    doesExist: (state) => ({ nextDoesExist: state }),
-  },
-  activeIndex: (state) => ({ activeIndex: state }),
-  selectionLength: (state) => ({ selectionLength: state }),
-}));
-jest.mock('data/redux/requests/selectors', () => ({
-  allowNavigation: (state) => ({ allowNavigation: state }),
-}));
+};
+
+const renderWithIntl = (component) => render(
+  <IntlProvider locale="en" messages={mockMessages}>
+    {component}
+  </IntlProvider>,
+);
 
 describe('SubmissionNavigation component', () => {
   describe('component', () => {
-    const props = {
-      intl: { formatMessage },
+    const defaultProps = {
+      intl: mockIntl,
       activeIndex: 4,
       selectionLength: 5,
+      loadNext: jest.fn(),
+      loadPrev: jest.fn(),
+      hasPrevSubmission: true,
+      hasNextSubmission: true,
+      allowNavigation: true,
     };
+
     beforeEach(() => {
-      props.loadNext = jest.fn().mockName('this.props.loadNext');
-      props.loadPrev = jest.fn().mockName('this.props.loadPrev');
+      defaultProps.loadNext.mockClear();
+      defaultProps.loadPrev.mockClear();
     });
-    test('snapshot: no prev submission (disabled)', () => {
-      expect(shallow(
-        <SubmissionNavigation {...props} activeIndex={0} hasNextSubmission />,
-      ).snapshot).toMatchSnapshot();
+
+    it('renders navigation with current position and total submissions', () => {
+      renderWithIntl(<SubmissionNavigation {...defaultProps} />);
+      expect(screen.getByText('FormattedMessage')).toBeInTheDocument();
     });
-    test('snapshot: no next submission (disabled)', () => {
-      expect(shallow(
-        <SubmissionNavigation {...props} hasPrevSubmission />,
-      ).snapshot).toMatchSnapshot();
+
+    it('disables previous button when no previous submission exists', () => {
+      renderWithIntl(
+        <SubmissionNavigation {...defaultProps} hasPrevSubmission={false} />,
+      );
+      const prevButton = screen.getByRole('button', { name: /load previous submission/i });
+      expect(prevButton).toBeDisabled();
+    });
+
+    it('disables next button when no next submission exists', () => {
+      renderWithIntl(
+        <SubmissionNavigation {...defaultProps} hasNextSubmission={false} />,
+      );
+      const nextButton = screen.getByRole('button', { name: /load next submission/i });
+      expect(nextButton).toBeDisabled();
+    });
+
+    it('disables both buttons when navigation is not allowed', () => {
+      renderWithIntl(
+        <SubmissionNavigation {...defaultProps} allowNavigation={false} />,
+      );
+      const prevButton = screen.getByRole('button', { name: /load previous submission/i });
+      const nextButton = screen.getByRole('button', { name: /load next submission/i });
+      expect(prevButton).toBeDisabled();
+      expect(nextButton).toBeDisabled();
+    });
+
+    it('calls loadPrev when previous button is clicked', () => {
+      renderWithIntl(<SubmissionNavigation {...defaultProps} />);
+      const prevButton = screen.getByRole('button', { name: /load previous submission/i });
+      fireEvent.click(prevButton);
+      expect(defaultProps.loadPrev).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls loadNext when next button is clicked', () => {
+      render(<SubmissionNavigation {...defaultProps} />);
+      const nextButton = screen.getByRole('button', { name: /load next submission/i });
+      fireEvent.click(nextButton);
+      expect(defaultProps.loadNext).toHaveBeenCalledTimes(1);
+    });
+
+    it('shows correct position when at first submission', () => {
+      render(
+        <SubmissionNavigation {...defaultProps} activeIndex={0} hasPrevSubmission={false} />,
+      );
+      expect(screen.getByText('FormattedMessage')).toBeInTheDocument();
     });
   });
-  describe('mapStateToProps', () => {
-    let mapped;
-    const testState = { some: 'test-state' };
-    beforeEach(() => {
-      mapped = mapStateToProps(testState);
-    });
-    test('activeIndex loads from grading.activeIndex', () => {
-      expect(mapped.activeIndex).toEqual(selectors.grading.activeIndex(testState));
-    });
-    test('hasNextSubmission loads from grading.next.doesExist', () => {
-      expect(mapped.hasNextSubmission).toEqual(selectors.grading.next.doesExist(testState));
-    });
-    test('hasPrevSubmission loads from grading.prev.doesExist', () => {
-      expect(mapped.hasPrevSubmission).toEqual(selectors.grading.prev.doesExist(testState));
-    });
-    test('selectionLength loads from grading.selectionLength', () => {
-      expect(mapped.selectionLength).toEqual(selectors.grading.selectionLength(testState));
-    });
-  });
+
   describe('mapDispatchToProps', () => {
-    it('loads loadNext from thunkActions.grading.loadNext', () => {
+    it('loads loadNext from thunk actions', () => {
       expect(mapDispatchToProps.loadNext).toEqual(thunkActions.grading.loadNext);
     });
-    it('loads loadPrev from thunkActions.grading.loadPrev', () => {
+
+    it('loads loadPrev from thunk actions', () => {
       expect(mapDispatchToProps.loadPrev).toEqual(thunkActions.grading.loadPrev);
     });
   });
