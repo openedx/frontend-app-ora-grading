d18608e47db958f58a94bc6afb2d266f89c0b66b
Victor <vm.navarro94@gmail.com>
2025-06-16 14:17:14 -0600

    test: deprecate react-unit-test-utils part-6

--- src/containers/ResponseDisplay/SubmissionFiles.test.jsx
+++ src/containers/ResponseDisplay/SubmissionFiles.test.jsx
@@ -1,99 +1,104 @@
-import React from 'react';
-import { shallow } from '@edx/react-unit-test-utils';
-
+import { render, screen } from '@testing-library/react';
+import { IntlProvider } from '@edx/frontend-platform/i18n';
 import { downloadAllLimit, downloadSingleLimit } from 'data/constants/files';
-
-import { formatMessage } from 'testUtils';
 import { SubmissionFiles } from './SubmissionFiles';
-import messages from './messages';
 
-jest.mock('./components/FileNameCell', () => jest.fn().mockName('FileNameCell'));
-jest.mock('./components/FileExtensionCell', () => jest.fn().mockName('FileExtensionCell'));
-jest.mock('./components/FilePopoverCell', () => jest.fn().mockName('FilePopoverCell'));
-jest.mock('./FileDownload', () => 'FileDownload');
+jest.unmock('@openedx/paragon');
+jest.unmock('react');
+jest.unmock('@edx/frontend-platform/i18n');
+
+jest.mock('./components/FileNameCell', () => jest.fn(({ value }) => <div>Name: {value}</div>));
+jest.mock('./components/FileExtensionCell', () => jest.fn(({ value }) => <div>Extension: {value}</div>));
+jest.mock('./components/FilePopoverCell', () => jest.fn(() => <div>Popover</div>));
+jest.mock('./FileDownload', () => jest.fn(({ files }) => <div data-testid="file-download">Download {files.length} files</div>));
 
 describe('SubmissionFiles', () => {
-  describe('component', () => {
-    const props = {
-      files: [
-        {
-          name: 'some file name.jpg',
-          description: 'description for the file',
-          downloadURL: '/valid-url-wink-wink',
-          size: 0,
-        },
-        {
-          name: 'file number 2.jpg',
-          description: 'description for this file',
-          downloadURL: '/url-2',
-          size: 0,
-        },
-      ],
-    };
-    let el;
-    beforeEach(() => {
-      el = shallow(<SubmissionFiles {...props} />);
+  const defaultProps = {
+    files: [
+      {
+        name: 'some file name.jpg',
+        description: 'description for the file',
+        downloadURL: '/valid-url-wink-wink',
+        size: 100,
+      },
+      {
+        name: 'file number 2.jpg',
+        description: 'description for this file',
+        downloadURL: '/url-2',
+        size: 200,
+      },
+    ],
+  };
+
+  const renderWithIntl = (component) => render(
+    <IntlProvider locale="en" messages={{}}>
+      {component}
+    </IntlProvider>,
+  );
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('behavior', () => {
+    it('displays submission files title with file count', () => {
+      renderWithIntl(<SubmissionFiles {...defaultProps} />);
+      const title = screen.getByTestId('submission-files-title');
+      expect(title).toBeInTheDocument();
+      expect(title).toHaveTextContent(`Submission Files (${defaultProps.files.length})`);
     });
 
-    describe('snapshot', () => {
-      test('files existed for props', () => {
-        expect(el.snapshot).toMatchSnapshot();
-      });
-
-      test('files does not exist', () => {
-        el = shallow(<SubmissionFiles {...props} files={[]} />);
-
-        expect(el.snapshot).toMatchSnapshot();
-      });
-      test('files size exceed', () => {
-        const files = props.files.map(file => ({ ...file, size: downloadSingleLimit + 1 }));
-        el = shallow(<SubmissionFiles {...props} files={files} />);
-        expect(el.snapshot).toMatchSnapshot();
-      });
+    it('renders file download component when files can be downloaded', () => {
+      renderWithIntl(<SubmissionFiles {...defaultProps} />);
+      const downloadComponent = screen.getByTestId('file-download');
+      expect(downloadComponent).toBeInTheDocument();
+      expect(downloadComponent).toHaveTextContent('Download 2 files');
     });
 
-    describe('behavior', () => {
-      test('title', () => {
-        const titleEl = el.instance.findByTestId('submission-files-title')[0].children[0];
-        expect(titleEl.el).toEqual(
-          `${formatMessage(messages.submissionFiles)} (${props.files.length})`,
-        );
-      });
+    it('displays warning when individual file exceeds size limit', () => {
+      const largeFileProps = {
+        ...defaultProps,
+        files: [
+          { ...defaultProps.files[0], size: downloadSingleLimit + 1 },
+          defaultProps.files[1],
+        ],
+      };
+      renderWithIntl(<SubmissionFiles {...largeFileProps} />);
 
-      describe('canDownload', () => {
-        test('normal file size', () => {
-          expect(el.instance.findByTestId('file-download')).toHaveLength(1);
-        });
+      expect(screen.queryByTestId('file-download')).not.toBeInTheDocument();
+      const warningText = screen.getByTestId('exceed-download-text');
+      expect(warningText).toBeInTheDocument();
+      expect(warningText).toHaveTextContent('Exceeded the allow download size');
+    });
 
-        test('one of the file exceed the limit', () => {
-          const oneFileExceed = [{ ...props.files[0], size: downloadSingleLimit + 1 }, props.files[1]];
+    it('displays warning when total file size exceeds limit', () => {
+      const largeFileSize = (downloadAllLimit + 1) / 20;
+      const largeFilesProps = {
+        ...defaultProps,
+        files: Array(20).fill({
+          name: 'large file.jpg',
+          description: 'large file description',
+          downloadURL: '/large-file-url',
+          size: largeFileSize,
+        }),
+      };
+      renderWithIntl(<SubmissionFiles {...largeFilesProps} />);
 
-          oneFileExceed.forEach(file => expect(file.size < downloadAllLimit).toEqual(true));
+      expect(screen.queryByTestId('file-download')).not.toBeInTheDocument();
+    });
 
-          el = shallow(<SubmissionFiles {...props} files={oneFileExceed} />);
-          expect(el.instance.findByTestId('file-download')).toHaveLength(0);
+    it('displays title only when no files are provided', () => {
+      const { container } = renderWithIntl(<SubmissionFiles {...defaultProps} files={[]} />);
+      const title = container.querySelector('.submission-files-title h3');
+      expect(title).toBeInTheDocument();
+      expect(title).toHaveTextContent('Submission Files (0)');
+      expect(screen.queryByTestId('file-download')).not.toBeInTheDocument();
+    });
 
-          const warningEl = el.instance.findByTestId('exceed-download-text')[0];
-          expect(warningEl.el.children[1]).toEqual(formatMessage(messages.exceedFileSize));
-        });
-
-        test('total file size exceed the limit', () => {
-          const length = 20;
-          const totalFilesExceed = new Array(length).fill({
-            name: 'some file name.jpg',
-            description: 'description for the file',
-            downloadURL: '/valid-url-wink-wink',
-            size: (downloadAllLimit + 1) / length,
-          });
-          totalFilesExceed.forEach(file => {
-            expect(file.size < downloadAllLimit).toEqual(true);
-            expect(file.size < downloadSingleLimit).toEqual(true);
-          });
-
-          el = shallow(<SubmissionFiles {...props} files={totalFilesExceed} />);
-          expect(el.instance.findByTestId('file-download')).toHaveLength(0);
-        });
-      });
+    it('renders data table with correct file information', () => {
+      const { container } = renderWithIntl(<SubmissionFiles {...defaultProps} />);
+      const dataTable = container.querySelector('.submission-files-table');
+      expect(dataTable).toBeInTheDocument();
     });
   });
 });
