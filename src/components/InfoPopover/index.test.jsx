480262a7a28a00d79bbcdf1712110d049512eb93
Victor Navarro <vm.navarro94@gmail.com>
2025-08-29 14:22:02 -0600

    test: deprecate react-unit-test-utils part-10 (#447)
    * test: deprecate react-unit-test-utils part-10
    
    * test: change fireEvent to userEvent
    
    ---------
    
    Co-authored-by: diana-villalvazo-wgu <diana.villalvazo@wgu.edu>

--- src/components/InfoPopover/index.test.jsx
+++ src/components/InfoPopover/index.test.jsx
@@ -1,4 +1,5 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { InfoPopover } from '.';
 
 jest.unmock('@openedx/paragon');
@@ -18,13 +19,14 @@ describe('Info Popover Component', () => {
       expect(getByTestId('esg-help-icon')).toBeInTheDocument();
     });
 
-    it('calls onClick when the help icon is clicked', () => {
-      const { getByTestId } = render(
+    it('calls onClick when the help icon is clicked', async () => {
+      render(
         <InfoPopover onClick={onClick}>
           {child}
         </InfoPopover>,
       );
-      fireEvent.click(getByTestId('esg-help-icon'));
+      const user = userEvent.setup();
+      await user.click(screen.getByTestId('esg-help-icon'));
       expect(onClick).toHaveBeenCalled();
     });
   });
