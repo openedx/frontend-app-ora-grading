9eab7901eb6c8e0cfb4b6fbbc33ac7f328ad7d13
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-09-04 09:45:09 -0600

    test: change fireEvent for userEvent

--- src/components/DemoAlert/index.test.jsx
+++ src/components/DemoAlert/index.test.jsx
@@ -1,4 +1,5 @@
-import { fireEvent } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithIntl } from '../../testUtils';
 
 import messages from './messages';
@@ -11,20 +12,21 @@ describe('DemoAlert component', () => {
   };
 
   it('does not render when isOpen is false', () => {
-    const { queryByText } = renderWithIntl(<DemoAlert {...props} isOpen={false} />);
-    expect(queryByText(messages.title.defaultMessage)).toBeNull();
+    renderWithIntl(<DemoAlert {...props} isOpen={false} />);
+    expect(screen.queryByText(messages.title.defaultMessage)).toBeNull();
   });
 
   it('renders with correct title and message when isOpen is true', () => {
-    const { getByText } = renderWithIntl(<DemoAlert {...props} />);
-    expect(getByText(messages.title.defaultMessage)).toBeInTheDocument();
-    expect(getByText(messages.warningMessage.defaultMessage)).toBeInTheDocument();
+    renderWithIntl(<DemoAlert {...props} />);
+    expect(screen.getByText(messages.title.defaultMessage)).toBeInTheDocument();
+    expect(screen.getByText(messages.warningMessage.defaultMessage)).toBeInTheDocument();
   });
 
-  it('calls onClose when confirmation button is clicked', () => {
-    const { getByText } = renderWithIntl(<DemoAlert {...props} />);
-    const confirmButton = getByText(messages.confirm.defaultMessage);
-    fireEvent.click(confirmButton);
+  it('calls onClose when confirmation button is clicked', async () => {
+    renderWithIntl(<DemoAlert {...props} />);
+    const user = userEvent.setup();
+    const confirmButton = screen.getByText(messages.confirm.defaultMessage);
+    await user.click(confirmButton);
     expect(props.onClose).toHaveBeenCalled();
   });
 });
